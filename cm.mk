0a3c2ba261450eb13595cb2faf8a8546b5e2bf3f
Aleksandr <bolt1502@mail.ru>
2017-05-31 18:23:49 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := SMART Surf LTE
+PRODUCT_RELEASE_NAME := Cirrus 9
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/MTS/SMART_Surf_4G/full_SMART_Surf_4G.mk)
+$(call inherit-product, device/fly/fs553/full_fs553.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := SMART_Surf_4G
-PRODUCT_NAME := lineage_SMART_Surf_4G
-PRODUCT_BRAND := MTS
-PRODUCT_MODEL := SMART_Surf_4G
-PRODUCT_MANUFACTURER := MTS
+PRODUCT_DEVICE := Cirrus 9
+PRODUCT_NAME := lineage_fs553
+PRODUCT_BRAND := FLY
+PRODUCT_MODEL := FS553
+PRODUCT_MANUFACTURER := FLY
