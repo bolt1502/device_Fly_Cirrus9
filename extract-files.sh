eba48983ed540ab8a87e64321c53488b5dd0b307
Aleksandr <bolt1502@mail.ru>
2017-05-31 18:53:28 +0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -5,8 +5,8 @@
 ## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
 ## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
 
-VENDOR=MTS
-DEVICE=SMART_Surf_4G
+VENDOR=fly
+DEVICE=fs553
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
